879e0b30f1ef090db8a93f0c7bb706e6a2e1ddac
Raihan <raihan.ruet.cse@gmail.com>
2020-12-13 21:12:54 +0600

    stateful added

---
+++ lib/test.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+class FirstClass extends StatefulWidget {
+  @override
+  _FirstClassState createState() => _FirstClassState();
+}
+
+class _FirstClassState extends State<FirstClass> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
